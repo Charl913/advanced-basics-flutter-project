6bb565f3cc03a0828e2d87d4a55f0d879069e5a6
Charl913 <charlkruger61@gmail.com>
2023-08-23 08:18:28 +0200

    Implement Expanded,  Combine Rows & Columns

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,7 +11,19 @@ class QuestionsSummary extends StatelessWidget {
       children: summaryData.map((data) {
         return Row(
           children: [
-            Text(((data['questions'] as int) + 1).toString()),
+            Text(((data['question_index'] as int) + 1).toString()),
+            Expanded(
+              child: Column(
+                children: [
+                  Text(data['question'] as String),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(data['user_answer'] as String),
+                  Text(data['correct_answer'] as String),
+                ],
+              ),
+            ),
           ],
         );
       }).toList(),
